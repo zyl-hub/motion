b22ab7abf1cced94ac26045fd7b9bc1ac9405484
APinkLemon <59688934+APinkLemon@users.noreply.github.com>
2021-01-10 14:53:55 +0800

    UPDATE_show_Img_4

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ from compute import *
 if __name__ == '__main__':
     x_data_list = loadData("v0_v1x", "1")
     y_data_list = loadData("v0_v1x", "1")
-    x, y, vx, vy, t = compute_2d(x_data_list, y_data_list)
+    x, y, vx, vy, ax, ay, t = compute_2d(x_data_list, y_data_list)
     showImg(x, vx, t, y, vy, 500, 0)
-    # showImg(x, vx, t, y, vy, 200, 1)
-    # showImg(x, vx, t, y, vy, 200, 2)
