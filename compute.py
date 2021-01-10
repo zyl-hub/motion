2d8f4c8c665101eec8977f1d45604a197c12a497
zyl_hub <1016076582@qq.com>
2021-01-10 12:08:31 +0800

    Add v_max

--- compute.py
+++ compute.py
@@ -2,10 +2,12 @@ from motionplan import compute_1d
 from motionplan import copy_sign
 
 
-def compute(x0, v0, a, frame_rate, aord):
+def compute(x0, v0, a, frame_rate, aord, v_max):
     # aord == 1 acc
     # aord == 0 dcc
     if aord == 1:
+        # if abs(v0) > v_max:
+        # a = 0
         v = v0 + copy_sign(a / frame_rate, v0)
     else:
         v = v0 - copy_sign(a / frame_rate, v0)
@@ -52,7 +54,7 @@ def compute_2d(x_data_list, y_data_list, y_v=None):
             x.append(x_data_list[0])
             v_x.append(x_data_list[2])
             info = compute(
-                x_data_list[0], x_data_list[2], 0, x_data_list[7], 1)
+                x_data_list[0], x_data_list[2], 0, x_data_list[7], 1, x_data_list[6])
             x_data_list[0] = info[0]
             x_data_list[2] = info[1]
             if(len(y_data_list) == 0):
@@ -69,7 +71,7 @@ def compute_2d(x_data_list, y_data_list, y_v=None):
                 y.append(x_data_list[0])
                 v_y.append(x_data_list[2])
                 info = compute(
-                    y_data_list[0], y_data_list[2], 0, y_data_list[7], 1)
+                    y_data_list[0], y_data_list[2], 0, y_data_list[7], 1, y_data_list[6])
                 y_data_list[0] = info[0]
                 y_data_list[2] = info[1]
         else:
@@ -77,7 +79,7 @@ def compute_2d(x_data_list, y_data_list, y_v=None):
             x.append(x_data_list[0])
             v_x.append(x_data_list[2])
             info = compute(x_data_list[0], x_data_list[2], x_last_all_info_dict["a"],
-                           x_data_list[7], x_last_all_info_dict["aord"])
+                           x_data_list[7], x_last_all_info_dict["aord"], x_data_list[6])
             compute_1d(x_data_list[1] - x_data_list[0],
                        x_data_list[2],
                        x_data_list[3],
@@ -94,7 +96,7 @@ def compute_2d(x_data_list, y_data_list, y_v=None):
                 y.append(x_data_list[0])
                 v_y.append(x_data_list[2])
                 info = compute(y_data_list[0], y_data_list[2], y_last_all_info_dict["a"], y_data_list[7],
-                               y_last_all_info_dict["aord"])
+                               y_last_all_info_dict["aord"], y_data_list[6])
                 compute_1d(y_data_list[1] - x_data_list[0],
                            y_data_list[2],
                            y_data_list[3],
