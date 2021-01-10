6d2067453b5c4bdfb25ba85c1a200688c9b37b4b
zyl_hub <1016076582@qq.com>
2021-01-10 18:29:28 +0800

    Add test_data

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from compute import *
 
 
 if __name__ == '__main__':
-    x_data_list = loadData("v0v1x", "5")
-    y_data_list = loadData("v0v1x", "5")
+    x_data_list = loadData("state2", "1")
+    y_data_list = loadData("state2", "1")
     x, y, vx, vy, ax, ay, t = compute_2d(x_data_list, y_data_list)
     showImg(x, vx, t, ax, y, vy, ay, 200, 0, render=True, debug=True)
     # showImg(x, vx, t, y, vy, 200, 1)
