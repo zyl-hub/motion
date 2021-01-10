01df79937e8e5dc10d13bfa8c7bec79763096e02
zyl_hub <1016076582@qq.com>
2021-01-10 14:33:55 +0800

    Add 小量小需啊唷

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from compute import *
 
 
 if __name__ == '__main__':
-    x_data_list = loadData("v0_v1x", "1")
-    y_data_list = loadData("v0_v1x", "1")
-    x, y, vx, vy, t = compute_2d(x_data_list, y_data_list)
-    showImg(x, vx, t, y, vy, 500, 0)
+    x_data_list = loadData("v0v1x", "5")
+    y_data_list = loadData("v0v1x", "5")
+    x, y, vx, vy, ax, ay, t = compute_2d(x_data_list, y_data_list)
+    showImg(x, vx, t, ax, y, vy, ay, 200, 0, render=True, debug=True)
     # showImg(x, vx, t, y, vy, 200, 1)
     # showImg(x, vx, t, y, vy, 200, 2)
