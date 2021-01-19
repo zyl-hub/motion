90f77162791989678090b69f7cfe1bf3f4567429
zyl_hub <1016076582@qq.com>
2021-01-19 23:20:45 +0800

    remove useless and compute.cpp

--- useless/CMmotion.h
+++
@@ -1,8 +0,0 @@
-#ifndef CMmotion_H_
-#define CMmotion_H_
-
-inline double copysign(double a, double b);
-
-void compute_motion_1d(double x, double v0, double v1, double a_max, double d_max, double v_max, double a_factor, double vel_factor, double& traj_accel, double& traj_time, double& traj_time_acc, double& traj_time_dec, double& traj_time_flat, double frame_rate);
-
-#endif
--- useless/compute.h
+++
@@ -1,6 +0,0 @@
-#ifndef compute_H_
-#define compute_H_
-
-void compute_2d(double* x_data_list, double* y_data_list, int epoch_num);
-
-#endif
